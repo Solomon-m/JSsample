19b0be58444880917035b50a629f39e68373c791
solomon-m <solomon.mark@a-cti.com>
2016-05-10 18:49:52 +0530

    add spreadSheetPush js file.

---
+++ spreadSheetPush.js
@@ -0,0 +1,18 @@
+(function() {
+    var userEmailId = "ashwin.mohan@a-cti.com";
+    var request = require('request');
+    try {
+        request('https://script.google.com/macros/s/AKfycbyDT7auYNBRQCKFXbIVgTQRDNpp_p_BRWhqI2wN9RA2s3ov8Y0/exec?userEmailId=' + userEmailId, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log('No Error....');
+                console.log('Server is Running');
+            }
+
+        });
+    } catch (e) {
+        console.log("Exception in saveUserInfoInSpreadSheet..!", e);
+        console.warn("Error stack :", e.stack);
+    }
+
+
+})();
