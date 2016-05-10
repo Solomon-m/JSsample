2afbb9e42961ff42c67f2fc389b8f46c6caa5d5a
solomon-m <solomon.mark@a-cti.com>
2016-05-10 20:49:47 +0530

    adding test.js

---
+++ test.js
@@ -0,0 +1,7 @@
+(function(){
+	console.log("Excuting IIFE...!");
+	console.log(__dirname);
+	console.log(__filename);
+	var oldappdirectory = __dirname;
+	console.log(" oldappdirectory : ",oldappdirectory);
+})();
